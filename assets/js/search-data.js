cb42a251e5e7f95a314748ad7d6dc6d75daff0cb
VictorLi5611 <73305287+VictorLi5611@users.noreply.github.com>
2025-04-06 02:13:49 +0000

    Deploying to gh-pages from @ VictorLi5611/VictorLi5611.github.io@cad8df2714aef82c399f77ad770e705b8bb3e440 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -493,35 +493,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%76%69%63%74%6F%72%6C%69%34@%63%6D%61%69%6C.%63%61%72%6C%65%74%6F%6E.%63%61", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/victorli5611", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://instagram.com/omg_its_victor_li", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/victorli5611", "_blank");
         },
       },{
       id: 'light-theme',
